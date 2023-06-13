e16c127e82a24c3292a51f2231769baeba0cbade
rej23 <rej.amada@gmail.com>
2023-06-13 11:43:42 +0100

    s

---
+++ T1/put.py
@@ -0,0 +1,7 @@
+user_name = input("what is your name: ")
+great_user = (f"hello {user_name}")
+
+    
+        
+
+        
---
+++ T1/tt.py
@@ -0,0 +1,68 @@
+
+def add(a,b):
+    
+    return a + b
+
+if __name__=="__main__":
+    
+    a = int(input("n1 = "))
+    b = int(input("n2 = "))
+    print(add(a,b))
+
+
+def ll():
+    n1 = 2
+    nn = 10
+    
+    return n1 , nn, n1 + nn
+
+c,n,f = ll()
+
+
+a = 444
+
+print(c,n,f,a)
+
+def lol():
+    n = [1,2,3]
+    n1 = 2
+    n2 = 2
+    
+    if n1 == n2:
+        return n1
+    
+
+
+
+
+
+
+####################  ( py -m unittest test_rps.py )
+
+
+
+# def lol():
+#     n1 = int(input(": "))
+#     nn = int(input(": "))
+    
+#     return n1 + nn
+
+# c = ll()
+
+# print(c)
+
+# import random
+
+# def play_game():
+#     choices = ["rock", "paper", "scissors"]
+#     player1_choice = input("pick: ")
+#     player2_choice = random.choice(choices)
+    
+#     print(f"Player 1 chose {player1_choice}")
+#     print(f"Player 2 chose {player2_choice}")
+    
+    
+    
+# play_game()
+
+ 
